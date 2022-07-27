bca63f63b362f57d05ad269a1d5147cc0633426e
mrum13 <muhammadrum135@.com>
2022-07-27 09:10:38 +0800

    add admin package

---
+++ app/src/main/java/com/autodhil/reminderin/ui/admin/AdminLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.autodhil.reminderin.ui.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.autodhil.reminderin.R
+
+class AdminLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_login)
+    }
+}
